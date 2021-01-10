82b0c0fa2480543f1b61d2ef8affab6feec2d4df
Werka <weronika.gajska@gmail.com>
2021-01-10 21:53:26 +0100

    Adding form to login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,20 @@
 import {Component} from "@angular/core";
+import {NgForm} from "@angular/forms";
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html'
 })
 export class LoginComponent {
+
+    onSubmit(form: NgForm) {
+        if (!form.valid) {
+            return;
+        }
+
+        console.log(form.value);
+
+        form.reset();
+    }
 
 }
