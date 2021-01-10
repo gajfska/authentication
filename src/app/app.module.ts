12309b9f738d1268b37282ac8292a0b615285c4b
Werka <weronika.gajska@gmail.com>
2021-01-10 14:59:24 +0100

    Adding form to auth.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {AuthComponent} from "./auth/auth.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import {AuthComponent} from "./auth/auth.component";
       AuthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
