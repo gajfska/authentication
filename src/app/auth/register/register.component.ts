a3e04fc1f47034e6677a4cf4d178c15c1180c845
Werka <weronika.gajska@gmail.com>
2021-01-10 21:55:29 +0100

    Adding form to register component

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,9 +1,20 @@
 import {Component} from "@angular/core";
+import {NgForm} from "@angular/forms";
 
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html'
 })
 export class RegisterComponent {
+
+    onSubmit(form: NgForm) {
+        if (!form.valid) {
+            return;
+        }
+
+        console.log(form.value);
+
+        form.reset();
+    }
 
 }
