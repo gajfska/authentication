5dce6594465ac19a43d3af82af259fd6e2aad544
Werka <weronika.gajska@gmail.com>
2021-01-10 21:41:30 +0100

    Adding routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import {RouterModule, Routes} from "@angular/router";
+import {NgModule} from "@angular/core";
+import {AuthComponent} from "./auth/auth.component";
+import {RegisterComponent} from "./auth/register/register.component";
+import {LoginComponent} from "./auth/login/login.component";
+
+const appRoutes: Routes = [
+    {path: '', redirectTo: '/auth', pathMatch: 'full' },
+    {path: 'auth', component: AuthComponent, children: [
+            { path: 'register', component: RegisterComponent},
+            { path: 'login', component: LoginComponent}
+        ]},
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {AuthComponent} from "./auth/auth.component";
 import {FormsModule} from "@angular/forms";
 import {RegisterComponent} from "./auth/register/register.component";
 import {LoginComponent} from "./auth/login/login.component";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {LoginComponent} from "./auth/login/login.component";
   ],
   imports: [
     BrowserModule,
-      FormsModule
+      FormsModule,
+      AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
