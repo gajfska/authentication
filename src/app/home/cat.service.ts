21b66f8803ab0b106b834a9cc98dde2c37ee6f84
Werka <weronika.gajska@gmail.com>
2021-01-11 22:58:49 +0100

    Added rx import

--- src/app/home/cat.service.ts
+++ src/app/home/cat.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Subscription} from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class CatService {
